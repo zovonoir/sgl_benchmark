dbe3e730961cfe5c9904e1fb25cb49aaa35f40ed
zovonoir <jialzhu@amd.com>
2026-06-04 05:01:55 +0000

    update confing

---
+++ dockers/launch.sh
@@ -0,0 +1,26 @@
+# vllm
+docker run -it --name zov-dsv4-perf \
+    --group-add=video \
+    --cap-add=SYS_PTRACE \
+    --security-opt seccomp=unconfined \
+    --device /dev/kfd \
+    --device /dev/dri \
+    -v /data/huggingface-cache/hub:/.cache/huggingface \
+	-v /home/jialzhu:/dockerx \
+	--cap-add=SYS_PTRACE --privileged  \
+	--device /dev/dri:/dev/dri --device /dev/kfd:/dev/kfd \
+    --ipc=host \
+	--network=host \
+	--entrypoint /bin/bash \
+    vllm/vllm-openai-rocm:nightly
+
+
+# container_env:
+#   - "HF_HOME=/.cache/huggingface/"
+#   - "HF_HUB_CACHE=/.cache/huggingface"
+#   - "VLLM_ROCM_USE_AITER=1"
+#   - "CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7"
+#   - "VLLM_ROCM_USE_AITER_MOE=1"
+#   - "SNAP=/.cache/huggingface/models--deepseek-ai--DeepSeek-V4-Pro/snapshots/89d501aed998d33fa4f4702102ec1bb2331e10f6"
+#   - "HF_HUB_OFFLINE=1"
+#   - "TRANSFORMERS_OFFLINE=1"
